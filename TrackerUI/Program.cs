5b53c71aa97dc6351d7937476cf8b4d27b56f435
Ali Adrian <alinasrat95@gmail.com>
2022-08-06 14:41:27 +0200

    Wired up the textfile for the PrizeModel

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -13,7 +13,7 @@ namespace TrackerUI
 		    ApplicationConfiguration.Initialize();
 
 		    // Initialize the database connections
-		    TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.Sql);
+		    TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.TextFile);
 
 		    Application.Run(new CreatePrizeForm());
 
